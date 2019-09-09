9aa9a5276414b6e3d2fe22080238873c87bc5d4d
Yuki-Shibanuma <smatosan@gmail.com>
2019-09-09 18:37:29 +0900

    signClick

--- src/main/java/me/mckd/neo/Worlds/ArrowWarp.java
+++ src/main/java/me/mckd/neo/Worlds/ArrowWarp.java
@@ -2,12 +2,17 @@ package me.mckd.neo.Worlds;
 
 import me.mckd.neo.Neo;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ArrowWarp implements Listener {
 
@@ -21,11 +26,30 @@ public class ArrowWarp implements Listener {
         if (!world.getName().equals("arrow")) {
             return;
         }
-        if (eve.getEntity()instanceof Arrow)  {
-            Arrow arrow = (Arrow)eve.getEntity();
+        if (eve.getEntity() instanceof Arrow) {
+            Arrow arrow = (Arrow) eve.getEntity();
+            Player p = (Player) eve.getEntity().getShooter();
             Location loc = arrow.getLocation();
-            Player p = (Player)eve.getEntity().getShooter();
             p.teleport(loc);
+        }
+    }
+
+    @EventHandler
+    public void signClick(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        Block b = e.getClickedBlock();
+        World w = p.getWorld();
+        if(
+            p.getWorld().getName().equals("arrow") &&
+            e.getAction().equals(Action.RIGHT_CLICK_BLOCK) &&
+            b.getType() == Material.SIGN_POST)
+        {
+            Sign sign = (Sign) b.getState();
+            String line = sign.getLine(1);
+            if( line.equals("STAGE 1")) {
+                Location loc = new Location(w, 100,20, 100);
+                p.teleport(loc);
+            }
         }
     }
 }
